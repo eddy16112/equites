a70ca963fdc326ee8295c3d4a67a77f2a265f574
George Stelle <stelleg@lanl.gov>
2017-06-20 15:11:58 -0600

    Simplified 2d

--- examples/2d.cc
+++ examples/2d.cc
@@ -5,15 +5,8 @@ task(void, copyRegion, r_region<float, 2> r1, w_region<float, 2> r2){
   for(auto i : r1) r2.write(i, r1.read(i)); 
 }
 
-/* To make take advantage of polymorphism, we can define polymorphic functions
-   and wrap monomorphic tasks around them. Plenty of room for improvement. */
-template <class a, size_t ndim> 
-void fill(w_region<a,ndim> r, a x){
-  for(auto i : r) r.write(i, x);
-}
-
 task(void, fillRegion, w_region<float, 2> r, float x){
-  fill(r, x); 
+  for(auto i : r) r.write(i, x);
 }
 
 task(void, printRegion, r_region<float, 2> r){
