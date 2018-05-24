f0d80b4f1bad910d466e50d98ff2acc859231ddb
Wei Wu <wwu12@saturn.icl.utk.edu>
2018-05-24 13:12:25 -0400

    hello

---
+++ examples/hello_world.cc
@@ -0,0 +1,23 @@
+#include <equites.h>
+using namespace equites;
+
+void hello_world(context c, int i)
+{
+  printf("hello world %d\n", i);
+}
+
+void top_level(context c)
+{
+  printf("top level\n");
+  runtime.execute_task(hello_world, c, 1);
+  runtime.execute_task(hello_world, c, 2);
+  runtime.execute_task(hello_world, c, 3);
+  runtime.execute_task(hello_world, c, 4);
+}
+
+int main(int argc, char** argv){
+  runtime.register_task<decltype(&top_level), top_level>("top_level");
+  runtime.register_task<decltype(&hello_world), hello_world>("hello_world");
+  runtime.start(top_level, argc, argv);
+}
+
